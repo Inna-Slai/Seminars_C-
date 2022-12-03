3680211f79ca5b05aa00c86aab01724dcc42685f
InnaSl <slaikovskaya@mail.ru>
2022-12-03 19:36:26 +0700

    Решение задачи номер 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+Console.WriteLine("Введите массив из 8 чисел");
+string input = Console.ReadLine() ??"";
+string[] temp = input.Split(' ');
+
+int[] resultArray = new int[temp.Length];
+for (int i = 0; i < resultArray.Length; i++)
+{
+    resultArray[i] = int.Parse(temp[i]);
+}
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.Write($"{resultArray[i]}, ");
+}
