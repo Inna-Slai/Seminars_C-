67e3f420471e53b45c6333f8513484eeead406ce
InnaSl <slaikovskaya@mail.ru>
2022-12-03 18:26:32 +0700

    Решение задачи номер 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число, а я скажиу не палиндром ли оно");
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+void YourNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - не является палиндромом.");
+}
+
+if (number!.Length == 5){
+  YourNumber(number);
+}
+else Console.WriteLine($"Введите число из пяти цифр");
