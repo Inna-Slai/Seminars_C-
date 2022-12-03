b04fffa09cc4bb311fe1a682e54d4b6a217eb204
InnaSl <slaikovskaya@mail.ru>
2022-12-03 15:35:49 +0700

    Решение задачи 28

---
+++ Zadacha28/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int X = Сomposition(num);
+Console.WriteLine($"Произведение чисел от 1 до N равно = {X}");
+
+int Сomposition(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+        fact = fact*i;
+    return fact;
+}
+
+
